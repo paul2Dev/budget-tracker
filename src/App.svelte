b5b0670d153ebfd02ba9d71d01d1616918ab51f9
Paul2D <paul.maxineanu@gmail.com>
2023-01-20 12:09:24 +0200

    add html entries examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,48 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div class="bg-white p-6 rounded-lg shadow-md">
+    <h2 class="text-lg font-medium mb-4">Budget Entries</h2>
+    <div class="w-full">
+      <div class="flex justify-between font-medium text-sm bg-gray-200 p-2 mb-2">
+          <div class="w-1/5">Name</div>
+          <div class="w-1/5">Amount</div>
+          <div class="w-1/5">Category</div>
+          <div class="w-1/5">Date</div>
+          <div class="w-1/5">Who Paid</div>
+      </div>
+      <div class="flex justify-between p-2 mb-2">
+          <div class="w-1/5">Salary</div>
+          <div class="w-1/5">$5,000</div>
+          <div class="w-1/5">Job</div>
+          <div class="w-1/5">01/01/2022</div>
+          <div class="w-1/5">Employer</div>
+      </div>
+      <div class="flex justify-between p-2 mb-2">
+          <div class="w-1/5">Rent</div>
+          <div class="w-1/5">$1,200</div>
+          <div class="w-1/5">Housing</div>
+          <div class="w-1/5">01/15/2022</div>
+          <div class="w-1/5">Self</div>
+      </div>
+      <div class="flex justify-between p-2 mb-2">
+          <div class="w-1/5">Utilities</div>
+          <div class="w-1/5">$300</div>
+          <div class="w-1/5">Bills</div>
+          <div class="w-1/5">01/20/2022</div>
+          <div class="w-1/5">Self</div>
+      </div>
+      <div class="flex justify-between p-2 mb-2">
+          <div class="w-1/5">Groceries</div>
+          <div class="w-1/5">$500</div>
+          <div class="w-1/5">Food</div>
+          <div class="w-1/5">01/25/2022</div>
+          <div class="w-1/5">Self</div>
+      </div>
+    </div>
+  </div>  
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+
