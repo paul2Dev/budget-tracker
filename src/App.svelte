26a761b9dc2f528de2e09bd2f7a93450789d6217
Paul2D <paul.maxineanu@gmail.com>
2023-01-20 12:33:19 +0200

    add example add entry form

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,45 @@
 </script>
 
 <main>
+  <form class="bg-white p-6 rounded-lg shadow-md">
+    <h2 class="text-lg font-medium mb-4">Add Budget Entry</h2>
+    <div class="flex gap-3 justify-left flex-wrap mb-4">
+      <div class="">
+        <label class="block text-gray-700 font-medium mb-2" for="name">Name</label>
+        <input class="bg-gray-200 p-2 rounded-lg w-full" type="text" id="name" name="name">
+      </div>
+      <div class="">
+        <label class="block text-gray-700 font-medium mb-2" for="amount">Amount</label>
+        <input class="bg-gray-200 p-2 rounded-lg w-full" type="number" id="amount" name="amount">
+      </div>
+      <div class="">
+        <label class="block text-gray-700 font-medium mb-2" for="category">Category</label>
+        <select class="bg-gray-200 p-2 rounded-lg w-full" id="category" name="category">
+          <option value="job">Job</option>
+          <option value="housing">Housing</option>
+          <option value="bills">Bills</option>
+          <option value="food">Food</option>
+        </select>
+      </div>
+      <div class="">
+        <label class="block text-gray-700 font-medium mb-2" for="date">Date</label>
+        <input class="bg-gray-200 p-2 rounded-lg w-full" type="date" id="date" name="date">
+      </div>
+      <div class="">
+        <label class="block text-gray-700 font-medium mb-2" for="who_paid">Who Paid</label>
+        <select class="bg-gray-200 p-2 rounded-lg w-full" id="who_paid" name="who_paid">
+          <option value="self">Self</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <div class="self-end">
+        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">Submit</button>
+      </div>
+    </div>
+  </form>
+  
+  
+
   <div class="bg-white p-6 rounded-lg shadow-md">
     <h2 class="text-lg font-medium mb-4">Budget Entries</h2>
     <div class="w-full">
